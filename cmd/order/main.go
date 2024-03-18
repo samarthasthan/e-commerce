7bde694865ff904823868fd2d2e8ae8eff8e6894
Samarth Asthan <samarthasthan@Samarths-MacBook-Air.local>
2024-03-19 02:09:30 +0530

    frontend build added

---
+++ cmd/order/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var port = ":8002"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	log.Printf("Broker listening on PORT %s", port)
+	log.Fatalln(http.ListenAndServe(port, mux))
+}
