3056733b4cb0515260fcb9339442002c061ceafb
root <samarthasthan27@gmail.com>
2024-04-20 12:57:32 +0000

    broker requests added to prometheus

--- go.mod
+++ go.mod
@@ -6,10 +6,16 @@ require google.golang.org/protobuf v1.33.0
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/prometheus/client_golang v1.19.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- internal/broker/delivery/rest/server.go
+++ internal/broker/delivery/rest/server.go
@@ -4,14 +4,49 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/samarthasthan/e-commerce/internal/broker/delivery/grpc"
 	"github.com/samarthasthan/e-commerce/pkg/logger"
 )
 
 var (
-	wg sync.WaitGroup
+	wg             sync.WaitGroup
+	requestCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests by method and path.",
+		},
+		[]string{"method", "path"},
+	)
+	requestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Histogram of response time in seconds by method and path.",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "path"},
+	)
 )
+
+func init() {
+	// Register the metrics with Prometheus
+	prometheus.MustRegister(requestCounter)
+	prometheus.MustRegister(requestDuration)
+}
+
+// MetricsMiddleware is a middleware that tracks request count and duration.
+func MetricsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+		next.ServeHTTP(w, r)
+		duration := time.Since(startTime).Seconds()
+		requestCounter.WithLabelValues(r.Method, r.URL.Path).Inc()
+		requestDuration.WithLabelValues(r.Method, r.URL.Path).Observe(duration)
+	})
+}
 
 type RestServer struct {
 	ServeMux *http.ServeMux
@@ -33,9 +68,11 @@ func NewRestServer(log *logger.Logger) *RestServer {
 
 func (r *RestServer) RunServer(PORT string) {
 	r.HandleRoutes()
+
+	wrappedServeMux := MetricsMiddleware(r.ServeMux)
 	wg.Add(1)
 	go func() {
-		err := http.ListenAndServe(fmt.Sprintf(":%v", PORT), r.ServeMux)
+		err := http.ListenAndServe(fmt.Sprintf(":%v", PORT), wrappedServeMux)
 		if err != nil {
 			r.log.Errorln(err)
 		}
@@ -50,4 +87,6 @@ func (r *RestServer) HandleRoutes() {
 		w.Write([]byte("Hello welcome to broker"))
 	})
 	r.ServeMux.Handle("POST /signup", LoggingMiddleware(r.log, http.HandlerFunc(r.handler.SignUp)))
+
+	r.ServeMux.Handle("/metrics", promhttp.Handler())
 }
