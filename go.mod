e3e3292105e1502acb272e4fc673add3b3d5f51a
root <samarthasthan27@gmail.com>
2024-04-27 11:26:05 +0000

    slightly change in auth grpc

--- go.mod
+++ go.mod
@@ -7,12 +7,14 @@ require google.golang.org/protobuf v1.33.0
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/boombuler/barcode v1.0.1-0.20190219062509-6c824513bacc // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/pquerna/otp v1.4.0 // indirect
 	github.com/prometheus/client_golang v1.19.0 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
@@ -24,6 +26,8 @@ require (
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
 	google.golang.org/grpc v1.63.2 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1 // indirect
 )
 
 require (
--- internal/authentication/delivery/grpc/server.go
+++ internal/authentication/delivery/grpc/server.go
@@ -11,30 +11,30 @@ import (
 )
 
 type AuthenticationGrpcServer struct {
-	Server grpc.Server
 	log    *logger.Logger
 	mysql  database.Database
 	redis  database.Database
+	server *grpc.Server
 }
 
-func NewAuthenticationGrpcServer(log *logger.Logger, mysql database.Database, redis database.Database) *AuthenticationGrpcServer {
-	return &AuthenticationGrpcServer{log: log, mysql: mysql, redis: redis}
+func NewAuthenticationGrpcServer(log *logger.Logger, mysql, redis database.Database) *AuthenticationGrpcServer {
+	server := grpc.NewServer()
+	return &AuthenticationGrpcServer{log: log, mysql: mysql, redis: redis, server: server}
 }
 
 func (g *AuthenticationGrpcServer) Run(port string) {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
 	if err != nil {
-		g.log.Error(err)
-		return
+		g.log.Fatalf("Failed to listen on port %s: %v", port, err)
 	}
+	defer listener.Close()
 
-	s := grpc.NewServer()
 	as := NewAuthenticationHandler(g.mysql, g.redis)
-	proto_go.RegisterAuthenticationServiceServer(s, as)
+	proto_go.RegisterAuthenticationServiceServer(g.server, as)
 
 	g.log.Infof("Authentication gRPC server listening on port %s", port)
-	err = s.Serve(listener)
+	err = g.server.Serve(listener)
 	if err != nil {
-		g.log.Error("Failed to serve gRPC server:", err)
+		g.log.Fatalf("Failed to serve gRPC server: %v", err)
 	}
 }
